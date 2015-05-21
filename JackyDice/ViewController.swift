28af431f6e8ec8a0a6cb49003795966fb1d7ac9b
jackywang135 <jackywangdeveloper@gmail.com>
2015-05-21 23:07:41 +0800

    Remove Ads.

--- JackyDice/ViewController.swift
+++ JackyDice/ViewController.swift
@@ -9,13 +9,11 @@
 import UIKit
 import AVFoundation
 import CoreMotion
-import iAd
 
-class ViewController: UIViewController, BottomViewDelegate, ADBannerViewDelegate, JW3DDiceViewDelegate {
+class ViewController: UIViewController, BottomViewDelegate, JW3DDiceViewDelegate {
     
     var diceView : JW3DDiceView?
     var bottomView : BottomView!
-    var adBannerView : ADBannerView!
     let animationBottomViewDuration = NSTimeInterval(0.5)
     
     override func viewDidLoad() {
@@ -41,7 +39,6 @@ extension ViewController {
     final private func setUpUI() {
         setUpDiceView()
         setUpBottomView()
-        setUpAdBannerView()
         animateBottomView()
     }
     final private func setUpBottomView() {
@@ -49,11 +46,6 @@ extension ViewController {
         bottomView.delegate = self
         updateTotalLabel()
         view.addSubview(bottomView)
-    }
-    final private func setUpAdBannerView() {
-        adBannerView = ADBannerView(frame:adHiddenFrame)
-        adBannerView.delegate = self
-        view.addSubview(adBannerView)
     }
     final private func setUpDiceView() {
         diceView = JW3DDiceView(frame: CGRectMake(0, 0, screenWidth, screenHeight - bottomViewHeight))
@@ -80,24 +72,6 @@ extension ViewController {
         bottomView.buttonShake.enabled = bool
         pop(bottomView.buttonShake)
     }
-    
-    final func showAd() {
-        UIView.animateWithDuration(0.5,
-            animations: {
-                self.diceView?.frame = animatorViewDuringAdFrame
-                self.bottomView.frame = bottomViewDuringAdFrame
-                self.adBannerView.frame = adShowingFrame
-        })
-    }
-    
-    final func hideAd() {
-        UIView.animateWithDuration(0.5,
-            animations: {
-                self.diceView?.frame = animatorViewHideAdFrame
-                self.bottomView.frame = bottomViewNormalFrame
-                self.adBannerView.frame = adHiddenFrame
-        })
-    }
     final func pop(view : UIView) {
         view.transform = CGAffineTransformScale(CGAffineTransformIdentity, 0.001, 0.001)
         UIView.animateWithDuration(0.2/1.5, animations: {view.transform = CGAffineTransformScale(CGAffineTransformIdentity, 1.1, 1.1)}, completion: {(complete : Bool) in
@@ -106,22 +80,6 @@ extension ViewController {
                 })
             })
         })}
-}
-
-//MARK: ADBannerViewDelegate
-extension ViewController {
-
-    final func bannerViewDidLoadAd(banner: ADBannerView!) {
-        showAd()
-    }
-    
-    final func bannerViewActionShouldBegin(banner: ADBannerView!, willLeaveApplication willLeave: Bool) -> Bool {
-        return true
-    }
-    
-    final func bannerView(banner: ADBannerView!, didFailToReceiveAdWithError error: NSError!) {
-        hideAd()
-    }
 }
 
 //MARK: JW3DDiceViewDelegate
@@ -157,31 +115,5 @@ extension ViewController {
     }
     final func pressedButtonShake(bottomView: BottomView) {
         diceView?.roll()
-    }
-}
-
-//MARK: iAd Testing
-extension ViewController {
-
-    final func testingButtonsForiAd() {
-        let diceWidth = diceView!.diceWidth
-        var buttonShowAd = UIButton.buttonWithType(UIButtonType.System) as! UIButton
-        buttonShowAd.setTitle("S", forState: UIControlState.Normal)
-        buttonShowAd.frame = CGRectMake(screenWidth - diceWidth, 0, diceWidth, diceWidth)
-        buttonShowAd.addTarget(self, action: "showAd", forControlEvents: UIControlEvents.TouchUpInside)
-        
-        var buttonHideAd = UIButton.buttonWithType(UIButtonType.System) as! UIButton
-        buttonHideAd.setTitle("H", forState: UIControlState.Normal)
-        buttonHideAd.frame = CGRectMake(screenWidth - diceWidth * 2, 0, diceWidth, diceWidth)
-        buttonHideAd.addTarget(self, action: "hideAd", forControlEvents: UIControlEvents.TouchUpInside)
-        
-        view.addSubview(buttonShowAd)
-        view.addSubview(buttonHideAd)
-        view.bringSubviewToFront(buttonShowAd)
-        view.bringSubviewToFront(buttonHideAd)
-        buttonHideAd.backgroundColor = UIColor.blackColor()
-        buttonShowAd.backgroundColor = UIColor.blackColor()
-        buttonHideAd.titleLabel!.textColor = UIColor.whiteColor()
-        buttonShowAd.titleLabel!.textColor = UIColor.whiteColor()
     }
 }
